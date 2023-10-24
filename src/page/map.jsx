c21247022588c30052d545b4342f47b2c69e5cf2
sunjae <zxsunje775@naver.com>
2023-10-24 19:30:56 +0900

    ㅁㄴㅇ

--- src/page/map.jsx
+++ src/page/map.jsx
@@ -1,47 +1,37 @@
 import React, { useEffect } from "react";
 import { styled } from "styled-components";
 
-const MainBtn = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 5rem;
-  color: blue;
-  background-color: orange;
-  font-size: 1rem;
-`;
+const apiKey = "759cc21177f7d8714e0d75a11877c4ab";
 
-const Map = () => {
-  const apiKey = "759cc21177f7d8714e0d75a11877c4ab";
-  // 페이지가 로드될 때 Kakao Maps 스크립트를 동적으로 로드
+function Map() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&autoload=false`;
-    script.async = true;
-    script.onload = initializeMap;
-    document.head.appendChild(script);
+    const apiKey = "759cc21177f7d8714e0d75a11877c4ab";
+
+    const mapScript = document.createElement("script");
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const mapContainer = document.getElementById("map");
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+          level: 3, // 지도의 확대 레벨
+        };
+        new window.kakao.maps.Map(mapContainer, mapOption);
+      });
+    };
+    mapScript.addEventListener("load", onLoadKakaoMap);
   }, []);
 
-  const initializeMap = () => {
-    if (window.kakao && window.kakao.maps) {
-      var container = document.getElementById("map");
-      var options = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667), // LatLng 생성자 사용
-        level: 3,
-      };
-
-      var map = new window.kakao.maps.Map(container, options);
-    }
-  };
-
   return (
-    <>
-      <h1>Map Page</h1>
-      <MainBtn id="map">둘러보기</MainBtn>
-      <div id="map" style={{ width: "100%", height: "400px" }}>
-        map
-      </div>
-    </>
+    <div>
+      <div id="map" className="w-96 h-96"></div>
+    </div>
   );
-};
+}
 
 export default Map;
