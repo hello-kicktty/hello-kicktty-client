f6c472581f52508ac2cb86453ac5c8a299ceb71d
sunjae <zxsunje775@naver.com>
2023-10-24 20:50:39 +0900

    지도 레벨 가져오기 실패(개발중)

--- src/page/map.jsx
+++ src/page/map.jsx
@@ -42,6 +42,47 @@ function Map() {
         };
         const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
+        function displayLevel() {
+          if (map) {
+            const levelEl = document.getElementById("maplevel");
+            const level = map.getLevel(); // 현재 지도 레벨을 가져옵니다
+
+            // 콘솔에 레벨을 출력합니다
+            console.log("현재 지도 레벨은 " + level + " 레벨 입니다.");
+
+            // HTML 엘리먼트에 레벨을 표시합니다
+            levelEl.innerHTML = "현재 지도 레벨은 " + level + " 레벨 입니다.";
+          }
+        }
+
+        // 지도 레벨은 지도의 확대 수준을 의미합니다
+        // 지도 레벨은 1부터 14레벨이 있으며 숫자가 작을수록 지도 확대 수준이 높습니다
+        function zoomIn() {
+          // 현재 지도의 레벨을 얻어옵니다
+          var level = map.getLevel();
+
+          // 지도를 1레벨 내립니다 (지도가 확대됩니다)
+          map.setLevel(level - 1);
+
+          // 지도 레벨을 표시합니다
+          displayLevel();
+        }
+
+        function zoomOut() {
+          // 현재 지도의 레벨을 얻어옵니다
+          var level = map.getLevel();
+
+          // 지도를 1레벨 올립니다 (지도가 축소됩니다)
+          map.setLevel(level + 1);
+
+          // 지도 레벨을 표시합니다
+          displayLevel();
+        }
+
+        function displayLevel() {
+          var levelEl = document.getElementById("maplevel");
+        }
+
         // 클릭 이벤트 핸들러 등록
         window.kakao.maps.event.addListener(
           map,
@@ -52,13 +93,14 @@ function Map() {
             const lng = latlng.getLng();
             console.log("클릭한 위치의 위도:", lat);
             console.log("클릭한 위치의 경도:", lng);
+            displayLevel();
           }
         );
 
         // 지도 타입 컨트롤 생성
         const mapTypeControl = new window.kakao.maps.MapTypeControl();
 
-        // 지도 타입 컨트롤을 지도에 표시
+        // 지도 타입 컨트롤을 지도에 표시 (지도/스카이뷰 선택 기능)
         map.addControl(
           mapTypeControl,
           window.kakao.maps.ControlPosition.TOPRIGHT
