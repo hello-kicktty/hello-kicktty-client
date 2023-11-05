9e6352591ac71aae5acf7fee020fb20ad56d6c22
lexxsh <john8538@naver.com>
2023-11-06 00:21:59 +0900

    기본세팅

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { Outlet } from "react-router-dom";
+const Background = styled.div`
+  width: 40vw;
+  background-color: white;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (max-width: 770px) {
+    width: 100%;
+  }
+`;
+
+
+function Layout(){
+    return(
+        <>
+        <Background>
+
+        <Outlet />
+        </Background>
+        </>
+    )
+}
+export default Layout;
