6411150734ddc2777783dc11cbacc83b0fb757c9
sunjae <zxsunje775@naver.com>
2023-11-07 14:17:10 +0900

    header개발중

---
+++ src/Components/header/header.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const HeaderBox = styled.div`
+  width: 100%;
+  height: fit-content;
+  background-color: wheat;
+`;
+
+const Header = () => {
+  return <HeaderBox>sadasd</HeaderBox>;
+};
+
+export default Header;
--- src/page/Main/Main.jsx
+++ src/page/Main/Main.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import Header from "../../Components/header/header";
 
 const Main = () => {
-    return (
-        <div>
-            이페이지는 MAIN입니다.
-        </div>
-    );
+  return (
+    <>
+      <Header></Header>
+      <div>이페이지는 MAIN입니다.</div>
+    </>
+  );
 };
 
 export default Main;
