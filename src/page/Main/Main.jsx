d745345ccbb279a99a5e04b93051cff756dd0c6a
lexxsh <john8538@naver.com>
2023-11-07 20:20:56 +0900

    main

--- src/page/Main/Main.jsx
+++ src/page/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Camera from '../../Components/Camera/Camera'
+import ScanBox from '../../Components/Box/ScanBox';
+import QRCodeScanner from '../../Components/Camera/Camera';
 const Main = () => {
     return (
         <div>
-            이페이지는 MAIN입니다.
-            <Camera></Camera>
+            <ScanBox>
+            </ScanBox>
+
         </div>
     );
 };
