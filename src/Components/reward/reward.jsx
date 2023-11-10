c4978ceba1fe5cb29b75df09cd3b4a935dc58346
sunjae <zxsunje775@naver.com>
2023-11-10 21:52:37 +0900

    reward페이지 완료

--- src/Components/reward/reward.jsx
+++ src/Components/reward/reward.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
 import { useNavigate } from "react-router-dom";
 import coin from "./coin.png";
+import stars from "./stars.png";
 
 const Container = styled.div`
   width: 100%;
@@ -10,7 +11,7 @@ const Container = styled.div`
   flex-direction: column;
   background: linear-gradient(rgba(211, 255, 82, 1), rgba(211, 255, 82, 0));
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
 `;
 
 const TextBox = styled.div`
@@ -19,6 +20,7 @@ const TextBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-top: 10rem;
 `;
 
 const Text1 = styled.div`
@@ -55,15 +57,37 @@ const HomeBtn = styled.button`
 const CoinImg = styled.img`
   width: 369px;
   height: 163px;
+  margin-top: 3rem;
+  margin-bottom: 3rem;
 `;
 
 // 애니메이션 효과 정의
 const slideInAnimation = keyframes`
   0% {
     width: 0;
+    height: 0;
   }
   100% {
-    width: 350px;
+    width: 309px;
+    height: 56px;
+  }
+`;
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const appearStars = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
   }
 `;
 
@@ -77,12 +101,40 @@ const SlideInPopup = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  top: 91px;
+  top: 121px;
   left: 50%;
   border-radius: 1rem;
   transform: translate(-50%, -50%);
-  animation: ${slideInAnimation} 0.5s ease-out; /* 애니메이션 적용 */
+  /* 애니메이션 적용 */
+  animation: ${slideInAnimation} 0.4s ease-out;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
+
+  /* 새로운 애니메이션 적용 */
+  &::before {
+    content: "주행이 완료되었습니다!";
+    opacity: 0;
+    animation: ${fadeIn} 0.4s ease-out 0.4s forwards; /* fadeIn animation with a delay */
+  }
+`;
+
+const Stars1 = styled.img`
+  width: 48px;
+  height: 48px;
+  position: absolute;
+  top: 121px;
+  left: 5%;
+  opacity: 0; /* Initially set opacity to 0 */
+  animation: ${appearStars} 0.5s ease-out 0.8s forwards;
+`;
+
+const Stars2 = styled.img`
+  width: 48px;
+  height: 48px;
+  position: absolute;
+  top: 60px;
+  left: 82%;
+  opacity: 0; /* Initially set opacity to 0 */
+  animation: ${appearStars} 0.5s ease-out 0.8s forwards;
 `;
 
 const Reward = (props) => {
@@ -102,7 +154,9 @@ const Reward = (props) => {
     <>
       <Container>
         {/* 애니메이션이 적용된 팝업 */}
-        {showPopup && <SlideInPopup>주행이 완료되었습니다!</SlideInPopup>}
+        {showPopup && <SlideInPopup />}
+        <Stars1 src={stars}></Stars1>
+        <Stars2 src={stars}></Stars2>
         <TextBox>
           <Text1>주차하기 완료!</Text1>
           <Text2>리워드를 확인해보세요!</Text2>
