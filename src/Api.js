61b91ade3ebcc787838ab18a055802060e015969
Ga Dong Sik <ret0422@gmail.com>
2023-11-18 01:13:05 +0900

    feat: deleteMethod 연동 완료(킥보드 대여)

--- src/Api.js
+++ src/Api.js
@@ -6,3 +6,8 @@ const BASE_URL = "http://13.124.82.89:54401";
 export function getKickList() {
     return axios.get(`${BASE_URL}/kickboards`).then((res) => res.data);
   }
+
+
+export function borrowKick(id) {
+    return axios.delete(`${BASE_URL}/kickboards/${id}`).then((res) => res.data);
+}
