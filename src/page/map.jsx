c435a200e39b2370400aa2f08c32e1525a348f75
sunjae <zxsunje775@naver.com>
2023-11-18 16:50:49 +0900

    map.jsx 내 위치 수정(고정위치값 유기)

--- src/page/map.jsx
+++ src/page/map.jsx
@@ -9,7 +9,7 @@ import { useState } from "react";
 import FirstInfo from "../Components/KickboardInfo/FirstInfo";
 import * as api from "../Api";
 import axios from "axios";
-import KicklayerData from "../ziko_restrictLayers.json"
+import KicklayerData from "../ziko_restrictLayers.json";
 const apiKey = "759cc21177f7d8714e0d75a11877c4ab";
 
 const MapBox = styled.div`
@@ -135,51 +135,76 @@ function Map() {
           position: map.getCenter(),
           image: UsermarkerImage,
         });
+        // HTML5의 geolocation으로 사용할 수 있는지 확인합니다
+        if (navigator.geolocation) {
+          // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+          navigator.geolocation.getCurrentPosition(function (position) {
+            var lat = position.coords.latitude, // 위도
+              lon = position.coords.longitude; // 경도
 
-        // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-        var iwContent = '<div style="padding:1px;">내위치</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-          iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+            var locPosition = new window.kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+              message = '<div style="padding:5px;">&_&헬로킥티</div>'; // 인포윈도우에 표시될 내용입니다
 
-        // 인포윈도우를 생성합니다
-        var infowindow = new window.kakao.maps.InfoWindow({
-          content: iwContent,
-          removable: iwRemoveable,
-        });
-        // 지도에 마커를 표시합니다
-        marker.setMap(map);
-
-        //console.log(data.clusters.length);
-
-        if (data.clusters && data.clusters.length > 0) {
-          var tmp = data.clusters;
-          
-          tmp.forEach(cluster => {
-            if(cluster.cluster_id == -1) return;
-            console.log(cluster.cluster_id)
-            var layerLatLng = []
-            cluster.borders.forEach(latlng => {
-              layerLatLng.push(new window.kakao.maps.LatLng(latlng.lat, latlng.lng))
-            })
-            var polygon = new window.kakao.maps.Polygon({
-              path: layerLatLng, // 그려질 다각형의 좌표 배열입니다
-              strokeWeight: 30, // 선의 두께입니다
-              strokeColor: '#39DE2A', // 선의 색깔입니다
-              strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-              strokeStyle: 'longdash', // 선의 스타일입니다
-              fillColor: '#39DE2A', // 채우기 색깔입니다
-              fillOpacity: 1 // 채우기 불투명도 입니다
-            });
-            // 지도에 다각형을 표시합니다
-            polygon.setMap(map);
+            // 마커와 인포윈도우를 표시합니다
+            displayMarker(locPosition, message);
           });
+        } else {
+          // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+          var locPosition = new window.kakao.maps.LatLng(33.450701, 126.570667),
+            message = "geolocation을 사용할수 없어요..";
+
+          displayMarker(locPosition, message);
         }
-        console.log(KicklayerData)
+
+        // 지도에 마커와 인포윈도우를 표시하는 함수입니다
+        function displayMarker(locPosition, message) {
+          // 고정마커를 생성합니다
+          var marker = new window.kakao.maps.Marker({
+            map: map,
+            position: locPosition,
+          });
+
+          var iwContent = message, // 인포윈도우에 표시할 내용
+            iwRemoveable = true;
+
+          // 인포윈도우를 생성합니다
+          var infowindow = new window.kakao.maps.InfoWindow({
+            content: iwContent,
+            removable: iwRemoveable,
+          });
+
+          // 클릭 이벤트 핸들러 등록
+          window.kakao.maps.event.addListener(
+            map,
+            "click",
+            function (mouseEvent) {
+              const latlng = mouseEvent.latLng;
+              const lat = latlng.getLat();
+              const lng = latlng.getLng();
+              console.log("클릭한 위치의 위도:", lat);
+              console.log("클릭한 위치의 경도:", lng);
+              // 마커 위치를 클릭한 위치로 옮깁니다
+              // marker.setPosition(latlng);
+              // 마커 위에 인포윈도우를 표시합니다
+              infowindow.open(map, marker);
+              displayLevel();
+            }
+          );
+
+          // 인포윈도우를 마커위에 표시합니다
+          infowindow.open(map, marker);
+
+          // 지도 중심좌표를 접속위치로 변경합니다
+          map.setCenter(locPosition);
+        }
+        console.log(KicklayerData);
         if (KicklayerData && KicklayerData.restrictLayers) {
           KicklayerData.restrictLayers.forEach((layerObj) => {
             const polygonPath = layerObj.layer.map((latlng) => {
               return new window.kakao.maps.LatLng(latlng.lat, latlng.lng);
             });
-        
+
             const polygon = new window.kakao.maps.Polygon({
               path: polygonPath,
               strokeWeight: 3,
@@ -189,23 +214,23 @@ function Map() {
               fillColor: "#F6B0A8",
               fillOpacity: 0.7,
             });
-        
+
             polygon.setMap(map); // Assuming 'map' is your kakao map instance
           });
         }
 
         function zoomIn() {
           var level = map.getLevel();
-  
+
           if (level > 5) {
             map.setLevel(level - 1);
             displayLevel();
           }
         }
-        
+
         function zoomOut() {
           var level = map.getLevel();
-        
+
           // Limit the maximum zoom level to 1
           if (level < 1) {
             map.setLevel(level + 1);
@@ -216,24 +241,6 @@ function Map() {
           const level = map.getLevel(); // 현재 지도 레벨을 가져옵니다
           console.log("현재 지도의 레벨 :" + level);
         }
-
-        // 클릭 이벤트 핸들러 등록
-        window.kakao.maps.event.addListener(
-          map,
-          "click",
-          function (mouseEvent) {
-            const latlng = mouseEvent.latLng;
-            const lat = latlng.getLat();
-            const lng = latlng.getLng();
-            console.log("클릭한 위치의 위도:", lat);
-            console.log("클릭한 위치의 경도:", lng);
-            // 마커 위치를 클릭한 위치로 옮깁니다
-            // marker.setPosition(latlng);
-            // 마커 위에 인포윈도우를 표시합니다
-            infowindow.open(map, marker);
-            displayLevel();
-          }
-        );
 
         // 이벤트 리스너 추가: 마우스 스크롤 이벤트
         const handleMouseWheel = (e) => {
